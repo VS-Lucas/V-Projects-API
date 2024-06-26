ae5284c18879ba37ad1aaa7f6fc80e8afcf6096c
Eric Barreto <ericbarreto521@gmail.com>
2024-06-26 15:41:04 -0300

    fix/user-return: returning user info for using in front-end context

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,6 +1,30 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Address } from "src/modules/user/dto/address.dto";
+
+class UserDto {
+    @ApiProperty()
+    id: number;
+
+    @ApiProperty()
+    email: string;
+
+    @ApiProperty()
+    name: string;
+
+    @ApiProperty()
+    role: string;
+
+    @ApiProperty()
+    position: string;
+
+    @ApiProperty({ type: Address })
+    address: Address;
+}
 
 export class AuthDto {
     @ApiProperty()
     accessToken: string;
+
+    @ApiProperty({ type: UserDto })
+    user: UserDto;
 }
--- src/modules/user/dto/address.dto.ts
+++ src/modules/user/dto/address.dto.ts
@@ -17,5 +17,5 @@ export class Address {
     country: string;
 
     @ApiProperty()
-    number: number;
+    number: string;
 }
