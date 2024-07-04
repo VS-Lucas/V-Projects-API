569b3472b8068c69e882d799890b1bb52e59121b
Vinícius Seabra <viniciusseabra226@gmail.com>
2024-07-04 11:35:42 -0300

    fix: create.user.dto

--- src/modules/equalization/equalization.module.ts
+++ src/modules/equalization/equalization.module.ts
@@ -4,6 +4,7 @@ import { EqualizationService } from './equalization.service';
 import { PrismaService } from 'src/database/prisma.service';
 import { SelfAssesmentService } from '../self-assesment/self-assesment.service';
 import { CyclesModule } from '../cycles/cycles.module';
+import { CyclesEqualizationModule } from '../cycles-equalization/cycles-equalization.module';
 
 @Module({
   imports: [CyclesModule, CyclesEqualizationModule],
